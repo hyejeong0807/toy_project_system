38237f989bdf19b259c5ee2a171b5167d85f9319
hyejeong-choi <hyejeong12311@gmail.com>
2024-02-27 16:37:41 +0900

    add: SIGSEG 시그널 추가

--- ui/input.c
+++ ui/input.c
@@ -1,15 +1,63 @@
 #include <stdio.h>
 #include <sys/prctl.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <ucontext.h>
+#include <unistd.h>
 
 #include <system_server.h>
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
 
+typedef struct _sig_ucontext {
+    unsigned long uc_flags;
+    struct ucontext *uc_link;
+    stack_t uc_stack;
+    struct sigcontext uc_mcontext;
+    sigset_t uc_sigmask;
+} sig_ucontext_t;
+
+void segfault_handler(int sig_num, siginfo_t *info, void *ucontext) {
+    void *array[50];
+    void *caller_address;
+    char **messages;
+    int size, i;
+    sig_ucontext_t *uc;
+
+    uc = (sig_ucontext_t *) ucontext;
+
+    caller_address = (void *) uc->uc_mcontext.rip;
+
+    fprintf(stderr, "\n");
+
+    if (sig_num == SIGSEGV)
+        printf("signal %d (%s), address is %p from %p\n", sig_num, strsignal(sig_num),
+               info->si_addr, (void *) caller_address);
+    else
+        printf("signal %d (%s)\n", sig_num, strsignal(sig_num));
+    
+    size = backtrace(array, 50);
+
+    free(messages);
+    exit(EXIT_FAILURE);
+}
+
 int input()
 {
     printf("나 input 프로세스!\n");
 
+    struct sigaction sa;
+    
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = segfault_handler;
+    sigemptyset(&sa.sa_mask);
+    if(sigaction(SIGSEGV, &sa, NULL) == -1) {
+        fprintf(stderr, "signal registration error\n");
+        exit(EXIT_FAILURE);
+    }
     while (1) {
         sleep(1);
     }
